76435d62d93f07bdb4fe3ebcd38dea1baaba9d57
Madman10K <stanv.contact176@gmail.com>
2025-07-01 01:57:59 +0300

    Added Metal custom renderer

---
+++ metal/metal.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include <Framework/Framework.hpp>
+
+namespace UImGuiRendererExamples
+{
+    class MetalRenderer final : public UImGui::GenericRenderer
+    {
+    public:
+        MetalRenderer() noexcept = default;
+        virtual void parseCustomConfig(YAML::Node& config) noexcept;
+
+        virtual void setupWindowIntegration() noexcept;
+        virtual void setupPostWindowCreation() noexcept;
+
+        virtual void init(UImGui::RendererInternalMetadata& metadata) noexcept;
+        virtual void renderStart(double deltaTime) noexcept;
+        virtual void renderEnd(double deltaTime) noexcept;
+        virtual void destroy() noexcept;
+
+        virtual void ImGuiNewFrame() noexcept;
+        virtual void ImGuiShutdown() noexcept;
+        virtual void ImGuiInit() noexcept;
+        virtual void ImGuiRenderData() noexcept;
+
+        // Only called on Vulkan, because there we need to wait for resources to be used before freeing resources,
+        // like textures
+        virtual void waitOnGPU() noexcept;
+
+        virtual ~MetalRenderer() noexcept = default;
+    private:
+        void* data = nullptr;
+        void* pool = nullptr;
+    };
+}
---
+++ metal/metal.mm
@@ -0,0 +1,125 @@
+#include "metal.hpp"
+
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_metal.h>
+
+#define GLFW_INCLUDE_NONE
+#define GLFW_EXPOSE_NATIVE_COCOA
+#include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
+
+#import <Metal/Metal.h>
+#import <QuartzCore/QuartzCore.h>
+
+extern "C" void* objc_autoreleasePoolPush(void);
+extern "C" void objc_autoreleasePoolPop(void* token);
+
+struct MetalRendererData
+{
+    id<MTLDevice> device;
+    id<MTLCommandQueue> commandQueue;
+    MTLRenderPassDescriptor* renderPassDescriptor;
+    CAMetalLayer* layer;
+    id<MTLCommandBuffer> commandBuffer;
+    id<CAMetalDrawable> drawable;
+    id<MTLRenderCommandEncoder> renderEncoder;
+};
+
+#define DATA CAST(MetalRendererData*, data)
+
+void UImGuiRendererExamples::MetalRenderer::parseCustomConfig(YAML::Node& config) noexcept
+{
+
+}
+
+void UImGuiRendererExamples::MetalRenderer::setupWindowIntegration() noexcept
+{
+    data = new MetalRendererData{};
+    UImGui::RendererUtils::setupManually();
+}
+
+void UImGuiRendererExamples::MetalRenderer::setupPostWindowCreation() noexcept
+{
+    DATA->device = MTLCreateSystemDefaultDevice();
+    DATA->commandQueue = [DATA->device newCommandQueue];
+}
+
+void UImGuiRendererExamples::MetalRenderer::init(UImGui::RendererInternalMetadata& metadata) noexcept
+{
+    metadata.vendorString = [DATA->device.name UTF8String];
+    metadata.gpuName = [DATA->device.name UTF8String];
+
+    Logger::log("Running on device - ", ULOG_LOG_TYPE_NOTE, metadata.gpuName);
+}
+
+void UImGuiRendererExamples::MetalRenderer::renderStart(double deltaTime) noexcept
+{
+    pool = objc_autoreleasePoolPush();
+    int width, height;
+    glfwGetFramebufferSize(UImGui::Window::getInternal(), &width, &height);
+    DATA->layer.drawableSize = CGSizeMake(width, height);
+    DATA->drawable = [DATA->layer nextDrawable];
+
+    DATA->commandBuffer = [DATA->commandQueue commandBuffer];
+
+    const auto& colours = ImGui::GetStyle().Colors[ImGuiCol_WindowBg];
+    DATA->renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(colours.x * colours.w, colours.y * colours.w, colours.z * colours.w, colours.w);
+    DATA->renderPassDescriptor.colorAttachments[0].texture = DATA->drawable.texture;
+    DATA->renderPassDescriptor.colorAttachments[0].loadAction = MTLLoadActionClear;
+    DATA->renderPassDescriptor.colorAttachments[0].storeAction = MTLStoreActionStore;
+
+    DATA->renderEncoder = [DATA->commandBuffer renderCommandEncoderWithDescriptor:DATA->renderPassDescriptor];
+    [DATA->renderEncoder pushDebugGroup:@"ImGui demo"];
+}
+
+void UImGuiRendererExamples::MetalRenderer::renderEnd(double deltaTime) noexcept
+{
+    [DATA->renderEncoder popDebugGroup];
+    [DATA->renderEncoder endEncoding];
+
+    [DATA->commandBuffer presentDrawable:DATA->drawable];
+    [DATA->commandBuffer commit];
+    objc_autoreleasePoolPop(pool);
+}
+
+void UImGuiRendererExamples::MetalRenderer::destroy() noexcept
+{
+    delete DATA;
+}
+
+void UImGuiRendererExamples::MetalRenderer::ImGuiNewFrame() noexcept
+{
+    ImGui_ImplMetal_NewFrame(DATA->renderPassDescriptor);
+    UImGui::RendererUtils::beginImGuiFrame();
+}
+
+void UImGuiRendererExamples::MetalRenderer::ImGuiShutdown() noexcept
+{
+    ImGui_ImplMetal_Shutdown();
+}
+
+void UImGuiRendererExamples::MetalRenderer::ImGuiInit() noexcept
+{
+    ImGui_ImplGlfw_InitForOpenGL(UImGui::Window::getInternal(), true);
+    ImGui_ImplMetal_Init(DATA->device);
+
+    NSWindow *nswin = glfwGetCocoaWindow(UImGui::Window::getInternal());
+    DATA->layer = [CAMetalLayer layer];
+    DATA->layer.device = DATA->device;
+    DATA->layer.pixelFormat = MTLPixelFormatBGRA8Unorm;
+    nswin.contentView.layer = DATA->layer;
+    nswin.contentView.wantsLayer = YES;
+
+    DATA->renderPassDescriptor = [MTLRenderPassDescriptor new];
+}
+
+void UImGuiRendererExamples::MetalRenderer::ImGuiRenderData() noexcept
+{
+    ImGui_ImplMetal_RenderDrawData(ImGui::GetDrawData(), DATA->commandBuffer, DATA->renderEncoder);
+}
+
+void UImGuiRendererExamples::MetalRenderer::waitOnGPU() noexcept
+{
+
+}
